f89b86b5fc2fe7c36e745a747785538d2791871d
jock-citrus <jock.dalby@citrusad.com>
2021-02-26 14:54:47 +1000

    fix e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', async () => {
+  it('should display tittle', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('my-news-app app is running!');
+    expect(await page.getTitleText()).toEqual('News stories');
   });
 
   afterEach(async () => {
